6be77e6114b2a176fcf34bbda0e62dfa3ebe7413
Rofl113 <OAPrivalov@yandex.ru>
2020-01-12 11:06:51 +0300

    [SessionApp] добавил название и размер окна

--- Core/SessionApp.cpp
+++ Core/SessionApp.cpp
@@ -8,7 +8,27 @@ SessionApp::SessionApp(const std::string& path)
 
 }
 
+void SessionApp::setName(const std::string& name)
+{
+	m_name = name;
+}
+
+void SessionApp::setSize(const std::pair<size_t, size_t>& size)
+{
+	m_size = size;
+}
+
 const std::string& SessionApp::getPath() const
 {
 	return m_path;
+}
+
+const std::string& SessionApp::getName() const
+{
+	return m_name;
+}
+
+const std::pair<size_t, size_t>& SessionApp::getSize() const
+{
+	return m_size;
 }
--- Core/SessionApp.h
+++ Core/SessionApp.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <utility>
 
 
 class SessionApp
@@ -9,8 +10,17 @@ class SessionApp
 public:
 	SessionApp(const std::string& path);
 
+public:
+	void setName(const std::string& name);
+	void setSize(const std::pair<size_t, size_t>& size);
+
+public:
 	const std::string& getPath() const;
+	const std::string& getName() const;
+	const std::pair<size_t, size_t>& getSize() const;
 
 private:
 	const std::string m_path;
+	std::string m_name;
+	std::pair<size_t, size_t> m_size;
 };
